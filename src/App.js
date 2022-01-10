477088e3ad9fd463e986b4352514d97cada44a0f
zdurr22 <zdurr22@gmail.com>
2022-01-10 12:11:48 -0500

    rendered board and added column checker

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 const width = 8
 const candyColors = [
   'blue',
@@ -9,20 +11,51 @@ const candyColors = [
 ]
 
 const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
   const createBoard = () => {
     const randomColorArrangement = []
     for (let i = 0; i < width * width; i++) {
       const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
       randomColorArrangement.push(randomColor)
     }
-    console.log(randomColorArrangement)
+    setCurrentColorArrangement(randomColorArrangement)
   }
 
-  createBoard()
-  return (
-    <div className="App">
+  useEffect(() => {
+    createBoard()
+  }, [])
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+
+  }, [checkForColumnOfThree, currentColorArrangement])
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
+          />
+        ))}
+      </div>
     </div>
   )
 }
